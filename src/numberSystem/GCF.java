1551fe3c4106ca1f46562d64a5702c9497a95ab2
shivrajparmar630 <shivrajparmar630@gmail.com>
2026-09-15 23:27:51 +0530

    add GCF solution

---
+++ src/numberSystem/GCF.java
@@ -0,0 +1,18 @@
+package numberSystem;
+
+public class GCF {
+
+        public static void main(String[] args) {
+            int a = 12;
+            int b = 18;
+
+            while (b != 0) {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+
+            System.out.println("GCD = " + a);
+        }
+    }
+
